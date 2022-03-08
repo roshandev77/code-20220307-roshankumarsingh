ea295d39679f8cca972fa3d9e100703beff5160d
Roshan Kumar <roshan.kumar@intersection.com>
2022-03-08 14:44:01 +0530

    Added the files for heroku deployments

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn  calculate_bmi:app
+heroku ps:scale web=1
--- calculate_bmi.py
+++ calculate_bmi.py
@@ -1,10 +1,13 @@
-from distutils.log import debug
+from distutils.log import ERROR, debug
+from http.client import OK
 from threading import Thread
 import flask
 from flask_cors import CORS
 import json
 
 input_data = {}
+OK_RESPONSE_CODE = {"responseCode": "200"}
+response_output = {}
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -20,28 +23,52 @@ def get_bmi():
         for data in input_data:
             bmi = round(data["WeightKg"] / (float(data["HeightCm"]/100))**2, 2)
             data["bmi"] = bmi 
-            # if bmi <= 18.4:
-            #     data["BMI Category"] = "Underweight" 
-            #     data["Health risk"] = "Malnutrition risk"
-            # elif bmi >=18.5 and bmi <= 24.9:
-            #     data["BMI Category"] = "Normalweight" 
-            #     data["Health risk"] = "Low risk"
-            # elif bmi>=25 and bmi<=29.9:
-            #     data["BMI Category"] = "Overweight" 
-            #     data["Health risk"] = "Enhanced risk"
-            # elif bmi>=30 and bmi<=34.9:
-            #     data["BMI Category"] = "Moderately obese" 
-            #     data["Health risk"] = "Medium risk"
-            # elif bmi>=35 and 40:
-            #     data["BMI Category"] = "Severely obese" 
-            #     data["Health risk"] = "High risk"
-            # elif bmi >= 40:
-            #     data["BMI Category"] = "Very severely obese" 
-            #     data["Health risk"] = "Very high risk"
+            if bmi <= 18.4:
+                data["BMI Category"] = "Underweight" 
+                data["Health risk"] = "Malnutrition risk"
+            elif bmi >=18.5 and bmi <= 24.9:
+                data["BMI Category"] = "Normalweight" 
+                data["Health risk"] = "Low risk"
+            elif bmi>=25 and bmi<=29.9:
+                data["BMI Category"] = "Overweight" 
+                data["Health risk"] = "Enhanced risk"
+            elif bmi>=30 and bmi<=34.9:
+                data["BMI Category"] = "Moderately obese" 
+                data["Health risk"] = "Medium risk"
+            elif bmi>=35 and 40:
+                data["BMI Category"] = "Severely obese" 
+                data["Health risk"] = "High risk"
+            elif bmi >= 40:
+                data["BMI Category"] = "Very severely obese" 
+                data["Health risk"] = "Very high risk"
         sample_return["data"] = input_data
     except Exception as e:
         print(e)
     return sample_return
+
+
+@app.route("/get-count")
+def get_count():
+    try:
+        overweight_count = 0
+        # get count of total number of overweight people
+        if "BMI Category" in input_data[0]:
+            print("Inside IF")
+            for dic in input_data:
+                print(dic)
+                if dic["BMI Category"] == "Overweight":
+                    print("here")
+                    overweight_count += 1
+            response_output["OverweightCount"] = overweight_count
+        else:
+            sample_data = get_bmi()
+            print("Sample Data",sample_data)
+            get_count()
+        print("Overweight", overweight_count)
+        return response_output
+    except Exception as e:
+        print(e)
+        return ERROR
 
 def instanciate_class():
     global input_data
@@ -50,7 +77,7 @@ def instanciate_class():
         input_data = json.load(input_file)
     except Exception as e:
         print(e)
-    return 1
+    return OK_RESPONSE_CODE
 
 if __name__ == '__main__':
     Thread(target=instanciate_class).start()
